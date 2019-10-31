fbf860cabf34099d7b749bd63a954c1b3b5246a8
Will Hinkelman <will@Wills-MacBook-Pro-2.local>
2019-10-31 15:22:29 -0700

    cache elements and choose rand square

--- js/main.js
+++ js/main.js
@@ -4,14 +4,37 @@
 
 /*----- app's state (variables) -----*/
 
-
+var simonArr, userChoice;
 
 /*----- cached element references -----*/
 
-
+const redSquare = document.getElementById('1');
+const greenSquare = document.getElementById('2');
+const blueSquare = document.getElementById('3');
+const purpleSquare = document.getElementById('4');
 
 /*----- event listeners -----*/
 
 
 
 /*----- functions -----*/
+
+init();
+
+function init() {
+    simonArr = [];
+    userChoice = null; 
+
+    render();
+}
+
+function render() {
+    simonPick();
+    displayPicks();
+}
+
+function simonPick() {
+    let randNum = Math.floor((Math.random() * 4) + 1)
+    simonArr.push(randNum);
+}
+
