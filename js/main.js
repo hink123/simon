16675661e8cba2a889326454cfc1fc37951bee93
Will Hinkelman <will@Wills-MacBook-Pro-2.local>
2019-11-01 14:36:51 -0700

    working comparison of choices

--- js/main.js
+++ js/main.js
@@ -5,20 +5,22 @@ const UNSELECTED_SQUARE = 1;
 
 /*----- app's state (variables) -----*/
 
-var simonArr, userChoice, board;
+var simonArr, userChoice, board, counter;
 
 /*----- cached element references -----*/
 
-const allSquares = document.querySelectorAll('section div');
+const allSquares = document.querySelector('section');
 const redSquare = document.getElementById('1');
 const greenSquare = document.getElementById('2');
 const blueSquare = document.getElementById('3');
 const purpleSquare = document.getElementById('4');
 const playButton = document.querySelector('button');
+const displayMessage = document.querySelector('h2');
 
 /*----- event listeners -----*/
 
 playButton.addEventListener('click', init);
+allSquares.addEventListener('click', userPicks);
 
 /*----- functions -----*/
 
@@ -32,12 +34,12 @@ function init() {
         3: blueSquare, 
         4: purpleSquare
     }
-    
-
+    displayMessage.textContent = "Good Luck!";
     render();
 }
 
 function render() {
+    counter = 0;
     simonPick();
     displayPicks();
 }
@@ -49,20 +51,38 @@ function simonPick() {
 
 function displayPicks(i = 0) {
     let pick = simonArr[i];
+
     setTimeout(function() {
-            // exits and resets last color
-            if(i === simonArr.length) {
-                board[simonArr[i - 1]].style.opacity = UNSELECTED_SQUARE;
-                return;
-            }
+        // exits and resets last color
+        if(i === simonArr.length) {
+            board[simonArr[i - 1]].style.opacity = null;
+            return;
+        }
         // resets colors
-        redSquare.style.opacity = UNSELECTED_SQUARE;
-        greenSquare.style.opacity = UNSELECTED_SQUARE;
-        blueSquare.style.opacity = UNSELECTED_SQUARE;
-        purpleSquare.style.opacity = UNSELECTED_SQUARE;
+        redSquare.style.opacity = null;
+        greenSquare.style.opacity = null;
+        blueSquare.style.opacity = null;
+        purpleSquare.style.opacity = null;
         
         //highlights pick
         board[pick].style.opacity = SELECTED_SQUARE;
         displayPicks(i + 1);
     }, 1000)
+}
+
+function userPicks(evt) {
+    if(displayMessage.textContent === 'Fail') {
+        return;
+    }
+    userChoice = evt.target;
+    let userChoiceAttr = parseInt(userChoice.getAttribute('id'));
+    if(userChoiceAttr === simonArr[counter]) {
+        counter += 1;
+        if(counter === simonArr.length) {
+            return render();
+        }
+    } else {
+        return displayMessage.textContent = 'Fail';
+    }
+    
 }
